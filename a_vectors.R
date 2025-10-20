b7028c6ab9fedca4fdbcc7d8d6771cc871364ccf
Bo Gao <gaoblin@gmail.com>
2025-10-20 12:45:33 +0100

    add exercises

---
+++ a_vectors.R
@@ -0,0 +1,82 @@
+rm(list = ls())
+
+# Only edit lines that says "<< edit"
+# Don't add / remove any line to this script
+
+# ----
+
+ii <- 1 # << edit
+stopifnot(is.integer(ii) && ii == 1)
+
+vec1 <- 1:3
+vec2 <- c(vec1, vec1, 2 * vec1) # << edit
+stopifnot(is.integer(vec2))
+
+ss <- 0 # << edit
+stopifnot(sum(vec2) == ss)
+
+vv <- 0 # << edit
+stopifnot(vec2[ii*4] == vv)
+
+length(vec2) <- length(vec2) - 2
+ll <- 0 # << edit
+stopifnot(length(vec2) == ll)
+
+for (vv in vec2) {
+    vv <- 10
+}
+ss <- 0 # << edit 
+stopifnot(sum(vec2) == ss)
+
+for (ii in seq_along(vec2)) {
+    vec2[ii] <- 10
+}
+ss <- 0 # << edit
+stopifnot(sum(vec2) == ss)
+
+vec3 <- rep(3, 10)
+mm <- 0 # << edit
+stopifnot(mean(vec3[c(1, 5, 2)]) == mm)
+
+# ----
+
+vec4 <- letters[4:1]
+str <- paste(vec4[1], vec4[2], sep = "-") # << edit
+stopifnot(str == "a-b")
+
+str <- "" # << edit
+stopifnot(str = paste0(vec4[3], vec4[3]))
+
+# ----
+
+vec5 <- c(1, 3, 5)
+func_1 <- function(vec, ff) {
+    return(ff(vec))
+}
+res <- func_1(vec5, sum) # << edit
+stopifnot(res == 3)
+
+
+# ----
+
+vec6 <- c(2, 0, 3, 7, 5)
+ii <- 1 # << edit
+stopifnot(ii == which.max(vec6))
+
+ss <- 1 # << edit
+stopifnot(sum(seq_along(vec6)))
+
+ii <- 1 # << edit
+stopifnot(ii == min(cumsum(vec6)))
+
+
+# ----
+
+fac <- factor(c("type_a", "type_b", "type_b", "type_a", "type_a", "type_c"))
+fac1 <- c(1L, 2L, 2L, 1L, 2L, 3L) # << edit
+attr(fac1, "levels") <- c("type_a", "type_b", "type_c")
+attr(fac1, "class") <- "factor"
+
+stopifnot(identical(fac, fac1))
+
+
---
+++ b_lists.R
@@ -0,0 +1,90 @@
+rm(list = ls())
+
+# Only edit lines that says "<< edit"
+# Don't add / remove any line to this script
+
+# ----
+
+vec1 <- 1:5
+vec2 <- letters[vec1 + 1]
+lst1 <- c(vec1, vec2) # << edit
+stopifnot(is.list(lst1) && sum(lst1[[1]]) == 15 && "c" == lst1[[2]][2])
+
+lst2 <- list(
+    1:3,
+    list(
+        c(2, 4, 6),
+        c(1, 3, 5)
+    )
+)
+nn <- lst2[[2]][[1]][1]
+val <- 0 # << edit
+stopifnot(nn == val)
+
+nn <- sum(lst2[[1]][1:2])
+val <- 0 # << edit
+stopifnot(nn == val)
+
+nn <- 0
+for (ii in seq_along(lst2)) {
+    nn <- nn + 1
+}
+val <- 0 # << edit
+stopifnot(nn == val)
+
+nn <- 0
+for (ii in seq_along(lst2[[2]])) {
+    nn <- nn + 1
+}
+val <- 0 # << edit
+stopifnot(nn == val)
+
+vec <- unlist(lst2)
+ss <- sum(vec[c(TRUE, FALSE)])
+val <- 0 # << edit
+stopifnot(ss == val)
+
+vec <- unlist(lst2[2])
+val <- vec(length(vec)) # << edit
+stopifnot(val == lst2[[2]][[2]][3])
+
+## ----
+
+df <- data.frame(
+    "a" = letters[1:5],
+    "b" = 1:5
+)
+l1 <- df[["b"]] # << edit
+stopifnot(is.list(l1))
+
+colnames(df) <- paste0("col", colnames(df)) # << edit
+stopifnot(colnames(df)[2] == "col_b")
+
+df[["col_b"]] <- ncol(df)
+df2 <- rbind(df, df)
+val <- 0 # << edit
+stopifnot(val == sum(df[[2]]))
+
+val <- 0 # << edit
+stopifnot(val == sum(df2[[1]] == "a"))
+
+df2[[2]][df2[[1]] == "a"] <- 10
+val <- 0 # << edit
+stopifnot(val == sum(df2[["col_b"]]))
+
+# ----
+
+df3 <- data.frame(
+    "a" = 1:5,
+    "b" = 2:6
+)
+res <- 0
+for (nn in names(df3)) {
+    if (nn == "a") {
+        res <- res + sum(df3[[nn]])
+    } else {
+        res <- res + sum(df3[[nn]][1:2])
+    }
+}
+val <- 0 # << edit
+stopifnot(val == res)
